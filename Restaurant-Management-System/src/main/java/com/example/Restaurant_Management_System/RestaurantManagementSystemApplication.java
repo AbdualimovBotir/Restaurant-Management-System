3892ba6b19e74c622d113d242faf24ef983d8bb2
AbdussomadMahkamov <mahkamovabdussomad@gmail.com>
2024-07-30 16:26:55 +0500

    spring boot

---
+++ Restaurant-Management-System/src/main/java/com/example/Restaurant_Management_System/RestaurantManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Restaurant_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantManagementSystemApplication.class, args);
+	}
+
+}
---
+++ Restaurant-Management-System/src/test/java/com/example/Restaurant_Management_System/RestaurantManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Restaurant_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
