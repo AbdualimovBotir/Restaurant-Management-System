9c47ab766bf70b8149778a8f10604e84655d5e99
AbdussomadMahkamov <mahkamovabdussomad@gmail.com>
2024-08-23 15:57:53 +0500

    Created menu controller and service. CRUD

---
+++ Restaurant-Management-System/src/main/java/com/example/Restaurant_Management_System/payload/MenuDto.java
@@ -0,0 +1,13 @@
+package com.example.Restaurant_Management_System.payload;
+
+import lombok.Data;
+
+@Data
+public class MenuDto {
+    private Long foodInfoId;
+    private String details;
+}
+/*
+"foodInfoId": 1,
+"details": "Lorem ipsum"
+ */
